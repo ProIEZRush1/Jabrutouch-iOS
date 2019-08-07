de422eba73e2ebf4cd8f2706db8cd06abf8274be
Aaron Tuil <aaront@ravtech.co.il>
2019-08-07 16:04:12 +0300

    Main Storyboard changes!!! Main screen shows image if no history, else: two tables.

---
+++ Jabrutouch/Controller/Main/Main/MainCollectionCellViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MainCollectionCellViewController.swift
+//  Jabrutouch
+//
+//  Created by Aaron Tuil on 07/08/2019.
+//  Copyright © 2019 Ravtech. All rights reserved.
+//
+
+import Foundation
