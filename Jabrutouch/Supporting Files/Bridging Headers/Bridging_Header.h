ce6983a7fca3500aee60276011fbde0c034c52ec
Yoni Reiss <yoni@ravtech.co.il>
2019-08-21 14:50:31 +0300

    Added CocoaPods Added AWSS3 pod Added AWSS3Provider

---
+++ Jabrutouch/Supporting Files/Bridging Headers/Bridging_Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging_Header.h
+//  Jabrutouch
+//
+//  Created by Yoni Reiss on 21/08/2019.
+//  Copyright © 2019 Ravtech. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <AWSS3/AWSS3.h>
+
+#endif /* Bridging_Header_h */
