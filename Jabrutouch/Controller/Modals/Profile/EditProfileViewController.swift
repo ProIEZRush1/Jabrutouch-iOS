7a5ace353a7f5365cbd4b7ffc4c49bc330a175f0
Shlomo Carmen <shlomoc@ravtech.co.il>
2019-10-24 17:44:55 +0300

    Started to work on "edit profile"

--- Jabrutouch/Controller/Modals/Profile/EditProfileViewController.swift
+++ Jabrutouch/Controller/Modals/Profile/EditProfileViewController.swift
@@ -8,24 +8,61 @@
 
 import UIKit
 
-class EditProfileViewController: UIViewController {
+class EditProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
+    @IBOutlet weak var tableView: UITableView!
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func backPressed(_ sender: Any) {
+           dismiss(animated: true, completion: nil)
+       }
+       
+    @IBAction func doneButtonPressed(_ sender: Any) {
+        
     }
-    */
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 1
+        case 1:
+            return 1
+        default:
+            return 1
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.section {
+        case 0:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "completionProgress") as? CompletionProgressCell else { return UITableViewCell() }
+            let count = 60.0
+            let progress = CGFloat(count/100)
+            cell.completionPercentage.text = "\(Int(count))% Full"
+            cell.progressView.progress = progress
+            return cell
+        case 1:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "InterestsCell") as? InterestsCell else { return UITableViewCell() }
+            
+            return cell
+        default:
+            return UITableViewCell()
+        }
+        
+    }
+    
+    
+
 
 }
