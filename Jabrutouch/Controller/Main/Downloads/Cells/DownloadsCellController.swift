f2a37dfac34989bc43cf65d14444db30d4e28f2d
Shlomo Carmen <shlomoc@ravtech.co.il>
2019-11-26 13:44:13 +0200

    added progress bar to mishna viewed lessons

--- Jabrutouch/Controller/Main/Downloads/Cells/DownloadsCellController.swift
+++ Jabrutouch/Controller/Main/Downloads/Cells/DownloadsCellController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class DownloadsCellController: UITableViewCell {
     
+    @IBOutlet weak var progressBar: JBProgressBar!
     @IBOutlet weak var cellView: UIView!
     @IBOutlet weak var book: UILabel!
     @IBOutlet weak var chapter: UILabel!
