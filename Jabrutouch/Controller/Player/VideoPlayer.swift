eba81cb1c072f6a29f2936ed53d377a8730a661d
Shlomo Carmen <shlomoc@ravtech.co.il>
2019-12-05 14:59:46 +0200

    changed previous button to go back in 60 second's apter the lesson part

--- Jabrutouch/Controller/Player/VideoPlayer.swift
+++ Jabrutouch/Controller/Player/VideoPlayer.swift
@@ -426,13 +426,13 @@ class VideoPlayer: UIView {
         case .regular, .fullScreen:
             let sortedArray = self.videoParts.sorted{$0 > $1}
             for part in sortedArray {
-                if self.currentTime > part {
+                if self.currentTime > part + 60 {
                     self.setCurrentTime(part)
                     break
                 }
             }
             if let lastPart = sortedArray.last {
-                if self.currentTime < lastPart  {
+                if self.currentTime < lastPart + 60 {
                     self.setCurrentTime(0.0)
                 }
                 
