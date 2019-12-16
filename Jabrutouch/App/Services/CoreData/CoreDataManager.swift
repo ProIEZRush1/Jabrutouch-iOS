0ecd91c4e8146582407ecc6b0660ef9245961a62
Shlomo Carmen <shlomoc@ravtech.co.il>
2019-12-16 18:46:02 +0200

    Added core data model and manager

---
+++ Jabrutouch/App/Services/CoreData/CoreDataManager.swift
@@ -0,0 +1,132 @@
+//
+//  CoreDataManager.swift
+//  Jabrutouch
+//
+//  Created by Shlomo Carmen on 16/12/2019.
+//  Copyright © 2019 Ravtech. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataManager {
+    
+    private static var manager: CoreDataManager?
+    
+    private init() {
+        
+    }
+    
+    class var shared: CoreDataManager {
+        if self.manager == nil {
+            self.manager = CoreDataManager()
+        }
+        return self.manager!
+    }
+    
+    var allChats: [JTChatMessage] = []
+    
+    let managedContext = appDelegate.persistentContainer.viewContext
+    
+    func seveChat(chat: JTChatMessage) {
+        let chatEntity = NSEntityDescription.entity(forEntityName: "Chat", in: managedContext)
+        let newChat = NSManagedObject(entity: chatEntity!, insertInto: managedContext)
+        
+        newChat.setValue(chat.chatId, forKey: "chatId")
+        newChat.setValue(chat.title, forKey: "title")
+        newChat.setValue(chat.toUser, forKey: "toUser")
+        newChat.setValue(chat.createdDate, forKey: "creatAtDate")
+        newChat.setValue(chat.lastMessageTime, forKey: "lastMessageTime")
+        newChat.setValue(chat.lastMessage, forKey: "lastMessage")
+        newChat.setValue(chat.fromUser, forKey: "fromUser")
+        newChat.setValue(chat.chatType, forKey: "chatType")
+        newChat.setValue(chat.image, forKey: "userImage")
+        newChat.setValue(chat.read, forKey: "read")
+        
+        do{
+            try managedContext.save()
+        }
+        catch {
+            print("failed")
+        }
+        
+    }
+    
+    func saveMessage(message: JTMessage) {
+        let messageEntity = NSEntityDescription.entity(forEntityName: "Message", in: managedContext)
+        let newMessage = NSManagedObject(entity: messageEntity!, insertInto: managedContext)
+        
+        newMessage.setValue(message.chatId, forKey: "chatId")
+        newMessage.setValue(message.image, forKey: "userImage")
+        newMessage.setValue(message.toUser, forKey: "toUser")
+        newMessage.setValue(message.title, forKey: "title")
+        newMessage.setValue(message.sentDate, forKey: "sendAtDate")
+        newMessage.setValue(message.messageType, forKey: "messageType")
+        newMessage.setValue(message.messageId, forKey: "messageId")
+        newMessage.setValue(message.message, forKey: "message")
+        newMessage.setValue(message.fromUser, forKey: "fromUser")
+        newMessage.setValue(message.read, forKey: "read")
+        newMessage.setValue(message.isMine, forKey: "isMine")
+        
+        do{
+            try managedContext.save()
+        }
+        catch {
+            print("failed")
+        }
+    }
+    
+    func getAllChats() {
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Chat")
+        request.returnsObjectsAsFaults = false
+        do {
+            let result = try managedContext.fetch(request)
+            self.allChats = (result as! [NSManagedObject]).compactMap{JTChatMessage(values: $0)}
+            
+            
+        } catch {
+            
+            print("Failed")
+        }
+    }
+    
+    func getMessagesByChatId(id: Int) -> [JTMessage] {
+
+        let predicate = NSPredicate(format: "chatId = %i", id)
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Message")
+        request.predicate = predicate
+        request.returnsObjectsAsFaults = false
+        do {
+            
+            let result = try managedContext.fetch(request)
+            
+            return (result as! [NSManagedObject]).compactMap{JTMessage(values: $0)}
+            
+        } catch {
+            print("Failed")
+            return []
+        }
+    }
+    
+    func updateMessageById(message: JTMessage) {
+        let predicate = NSPredicate(format: "messageId = %i", message.messageId)
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Message")
+        request.predicate = predicate
+        request.returnsObjectsAsFaults = false
+        do {
+            
+            let result = try managedContext.fetch(request)
+            (result as! [NSManagedObject]).first?.setValue(message.read, forKey: "read")
+            do{
+                try managedContext.save()
+            }
+            catch {
+                print("failed")
+            }
+            
+        } catch {
+            print("Failed")
+            
+        }
+    }
+}
