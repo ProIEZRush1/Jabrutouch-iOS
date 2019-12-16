e84ff55e198804079586e73dd6b403db1325efa4
Shlomo Carmen <shlomoc@ravtech.co.il>
2019-12-16 18:50:13 +0200

    Added core data and notifications functions

--- Jabrutouch/App/Core/AppDelegate.swift
+++ Jabrutouch/App/Core/AppDelegate.swift
@@ -53,32 +53,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         let token = deviceToken.map{ String(format: "%02.2hhx", $0) }.joined()
         print("Notifications token: " + token)
-//        fcmToken = token
-
+        //        fcmToken = token
+        
     }
     
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
+    
     func application(_ application: UIApplication, handleEventsForBackgroundURLSession identifier: String, completionHandler: @escaping () -> Void) {
         AWSS3TransferUtility.interceptApplication(application, handleEventsForBackgroundURLSession: identifier, completionHandler: completionHandler)
     }
@@ -97,7 +97,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func registerForPushNotifications(application:UIApplication) {
-
+        
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) {
             (granted, error) in
             print("Permission granted: \(granted)")
@@ -126,23 +126,60 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }        
     }
+    // MARK: - Core Data stack
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        /*
+         The persistent container for the application. This implementation
+         creates and returns a container, having loaded the store for the
+         application to it. This property is optional since there are legitimate
+         error conditions that could cause the creation of the store to fail.
+         */
+        let container = NSPersistentContainer(name: "DataModel")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                
+                /*
+                 Typical reasons for an error here include:
+                 * The parent directory does not exist, cannot be created, or disallows writing.
+                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                 * The device is out of space.
+                 * The store could not be migrated to the current model version.
+                 Check the error message to determine what the actual problem was.
+                 */
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    // MARK: - Core Data Saving support
+    
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+    
+
 }
 
-    
+
 extension AppDelegate:UNUserNotificationCenterDelegate{
     
     func centerNotificationManager(){
         notificatioCenter.getDeliveredNotifications { (notifications:[UNNotification]) in
             for n in notifications{
-//                let userInfo = n.request.content.userInfo
-//                //print("## userInfo ## \(userInfo.debugDescription)")
-//                let remoteNotification = userInfo as! [String: AnyObject]
-                //print("## remoteNotification ## \(remoteNotification.debugDescription)")
-//                self.queue.sync {
-//
-//                    let _ = self.parsFCMNotification(messageInfo: n)
-//                }
-
                 print(n,"getDeliveredNotifications")
             }
         }
@@ -153,12 +190,43 @@ extension AppDelegate:UNUserNotificationCenterDelegate{
             }
         }
     }
-   
+    
     func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
         
     }
     
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+
+        // Step 1: Parse incoming content
+        let body = notification.request.content.body
+        if let data = body.data(using: String.Encoding.utf8) {
+            do {
+                if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String:Any] {
+                    guard let title = json["title"] as? String else { return }
+                    guard let message = json["message"] as? String else { return }
+//                    if let message = json["message"] as? String {
+//
+//                    }
+                }
+            } catch {
+                print("Something went wrong")
+            }
+        }
+        // Step 2 - Send local notification
+        self.sendUserNotification(body: "What ever you need or want", userInfo: ["again": "your choice"])
         completionHandler([.alert, .sound])
+    }
+    
+    private func sendUserNotification(body: String, userInfo: [String:Any]) {
+        let content = UNMutableNotificationContent()
+        content.body = body
+        content.userInfo = userInfo
+        
+        let request = UNNotificationRequest(identifier: "", content: content, trigger: nil)
+        UNUserNotificationCenter.current().add(request) { (error:Error?) in
+            if let error = error {
+                print("Failed adding notification request, with error: \(error)")
+            }
+        }
     }
 }
