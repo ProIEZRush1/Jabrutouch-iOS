209f26cfe1213894760ff6e3c4862177145fcb9f
Avi Deutsch <avrahamd@ravtech.co.il>
2020-04-27 14:10:28 +0300

    fixed donation alert

--- Jabrutouch/Controller/Player/DonatedAlert.swift
+++ Jabrutouch/Controller/Player/DonatedAlert.swift
@@ -45,20 +45,20 @@ class DonatedAlert: UIViewController {
     }
     
     override func viewDidLoad() {
-     //   super.viewDidLoad()
-       // self.setStrings()
-        //self.setCornerRadius()
-        //self.setShadow()
-//        self.setDonor()
-//        self.getDonorText()
+        super.viewDidLoad()
+        self.setStrings()
+        self.setCornerRadius()
+        self.setShadow()
+        //        self.setDonor()
+        //        self.getDonorText()
         // Do any additional setup after loading the view.
     }
     
     override func loadView() {
-       // Bundle.main.loadNibNamed("DonatedAlert", owner: self, options: nil)
-        Bundle.main.loadNibNamed("View", owner: self, options: nil)
+        // Bundle.main.loadNibNamed("DonatedAlert", owner: self, options: nil)
+        Bundle.main.loadNibNamed("DonatedAlert", owner: self, options: nil)
     }
-
+    
     //====================================================
     // MARK: - Setup
     //====================================================
@@ -70,7 +70,7 @@ class DonatedAlert: UIViewController {
         self.nameLabel.text = self.nameText
         self.startBtn.setTitle(Strings.donatedBtn, for: .normal)
     }
-
+    
     func setCornerRadius() {
         self.mainView.layer.cornerRadius = 31
         self.startBtn.layer.cornerRadius = 18
@@ -144,5 +144,5 @@ class DonatedAlert: UIViewController {
             self.delegate?.didDismiss()
         }
     }
-
+    
 }
