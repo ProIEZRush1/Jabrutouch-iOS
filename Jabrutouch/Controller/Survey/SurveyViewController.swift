c0df7a751b0217b9782bcde040bca5431cb09d09
Avraham Kirsch <avraham.kirsch@ravtech.co.il>
2021-10-19 18:01:44 +0300

    added slider to surveyVC for rating questions. created JTSurveyAnswerType created JTSurveyQuestionWithAnswerOptions created JTSurvey added functionality to nextButtonPressed(), switch to next question. added animation for when switching questions, flip card. created setLabelsForQuestion() to set ui for each question. created setupSurvey()
    working on checkboxe question ui, created CheckBoxListView class and  CheckBoxView class,

--- Jabrutouch/Controller/Survey/SurveyViewController.swift
+++ Jabrutouch/Controller/Survey/SurveyViewController.swift
@@ -14,6 +14,11 @@ class SurveyViewController: UIViewController, UIPickerViewDelegate, UIPickerView
     // MARK: Properties
     //===============================
     
+    var survey:JTSurvey = JTSurvey()
+    var currentQuestionIndex: Int = 0
+    var questionCount:Int {
+        return self.survey.questions.count
+    }
     var pickerData: [String] = []
     
     
@@ -29,6 +34,9 @@ class SurveyViewController: UIViewController, UIPickerViewDelegate, UIPickerView
     @IBOutlet weak var questionNumberLabel: UILabel!
     @IBOutlet weak var nextButton: UIButton!
     @IBOutlet weak var picker: UIPickerView!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var sliderValueLabel: UILabel!
+    @IBOutlet weak var checkListView: UIView!
     
     
     
@@ -44,8 +52,7 @@ class SurveyViewController: UIViewController, UIPickerViewDelegate, UIPickerView
         super.viewDidLoad()
         setupPicker()
         roundCorners()
-        let years = ["1900", "1950","2000", "2020"]
-        pickerData.append( contentsOf: years )
+        setupSurvey()
     }
     
     //===============================
@@ -60,9 +67,59 @@ class SurveyViewController: UIViewController, UIPickerViewDelegate, UIPickerView
     fileprivate func roundCorners() {
         Utils.setViewShape(view: self.cardView,  viewCornerRadius: 18)
         Utils.setViewShape(view: self.nextButton,  viewCornerRadius: 18)
+    }
+ 
+    func setupSurvey(){
+        let q1 = JTSurveyQuestionWithAnswerOptions(id: 0, question: "In what year were you born?", answerType: .picker, answerOptions: ["2000","2001","2002","2003","2004","2005"])
+        let q2 = JTSurveyQuestionWithAnswerOptions(id: 1, question: "Rate our app please!", answerType: .ratingBar, answerOptions: nil)
+        let q3 = JTSurveyQuestionWithAnswerOptions(id: 2, question: "When was your grandfather born?", answerType: .checkbox, answerOptions: ["1800","1801","1802","1803"])
+        self.survey.questions.append(q1)
+        self.survey.questions.append(q2)
+        self.survey.questions.append(q3)
+        
+        DispatchQueue.main.async {
+            self.setLabelsForQuestion(question: self.survey.questions[self.currentQuestionIndex], questionIndex: self.currentQuestionIndex)
+        }
 
+        
+        
     }
     
+    func setLabelsForQuestion(question:JTSurveyQuestionWithAnswerOptions, questionIndex: Int){
+        DispatchQueue.main.async {
+            self.currentQuestionIndex = questionIndex
+            self.questionLabel.text = question.question
+            self.questionNumberLabel.text = "\(questionIndex + 1)/\(self.questionCount)"
+            
+            self.picker.isHidden = question.answerType != .picker
+            self.slider.isHidden = question.answerType != .ratingBar
+            self.sliderValueLabel.isHidden = question.answerType != .ratingBar
+            self.checkListView.isHidden = question.answerType != .checkbox
+            
+            switch question.answerType {
+            case .picker:
+                self.pickerData = question.answerOptions ?? []
+                self.picker.reloadAllComponents()
+            case .ratingBar:
+                self.slider.value = 5.5
+                self.sliderValueLabel.text = "5"
+            case .checkbox:
+                let checkList = CheckBoxListView(frame: self.checkListView.frame)
+                checkList.setupCheckBoxes(data: question.answerOptions ?? [] )
+                self.checkListView.addSubview(checkList)
+                self.view.layoutIfNeeded()
+            default:
+                break
+            }
+            
+            if self.currentQuestionIndex == self.questionCount - 1{
+                //last question
+                self.nextButton.setTitle("Finish!", for: .normal)
+                self.nextButton.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3647058904, blue: 0.6705882549, alpha: 1)
+            }
+        }
+      
+    }
     //===============================
     // MARK: Picker Delegate Functions
     //===============================
@@ -90,26 +147,51 @@ class SurveyViewController: UIViewController, UIPickerViewDelegate, UIPickerView
     // MARK: Actions
     //===============================
     @IBAction func nextButtonPressed(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
+        if self.currentQuestionIndex == self.questionCount - 1 {
+            self.dismiss(animated: true, completion: nil)
+            
+        } else {
+            self.currentQuestionIndex += 1
+                        DispatchQueue.main.async {
+                            self.setLabelsForQuestion(question: self.survey.questions[self.currentQuestionIndex], questionIndex: self.currentQuestionIndex)
+                            UIView.transition(with: self.cardView, duration: 1, options: .transitionFlipFromRight, animations: nil, completion: nil)
+                        }
+        }
+    }
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        self.sliderValueLabel.text = String(Int(sender.value))
     }
     
     
     
-    
-    
-    
-    
-    
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
+class JTSurvey {
+    var id: Int = 0
+    var title: String = "Survey 1"
+    var subtitleIntro:String = "Please answer these short questions so we can serve you better."
+    var questions:[JTSurveyQuestionWithAnswerOptions] = []
+}
+
+class JTSurveyQuestionWithAnswerOptions {
+    var id:Int
+    var question: String
+    var answerType:JTSurveyAnswerType
+    var answerOptions: [String]?
+    
+    init(id:Int, question:String, answerType:JTSurveyAnswerType, answerOptions: [String]?) {
+        self.id = id
+        self.question = question
+        self.answerType = answerType
+        self.answerOptions = answerOptions
+    }
+}
+enum JTSurveyAnswerType {
+    case picker
+    case checkbox
+    case ratingBar
+    case complexPicker
+}
+
